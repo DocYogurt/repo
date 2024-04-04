e914f805ad5f0083e9fa2efc4f843815d490fe96
DocYogurt <58424831+DocYogurt@users.noreply.github.com>
2024-04-05 00:09:42 +0800

    Update whitelists.lua

--- whitelists.lua
+++ whitelists.lua
@@ -1,4 +1,5 @@
 return {
-	"395beb52-53de-4abd-80d5-f3707ce0164f", -- MEEEEE
-	"89a4fe33-b015-4c0b-844d-668c9a80f35a" -- LONGSHU
+	"89a4fe33-b015-4c0b-844d-668c9a80f35a", -- LONGSHU
+	"395beb52-53de-4abd-80d5-f3707ce0164f" -- MEEEEE
+	
 }
